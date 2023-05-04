8cffddefb42333b28d29c0aef8a149763d9d6938
sebastienDOC <s.dechand21@gmail.com>
2023-05-04 17:20:21 +0200

    tri / filtres

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -5,14 +5,81 @@ fetch ("http://localhost:5678/api/works")
     })
     .then(function(projets) {
         let gallerie = document.querySelector(".gallery");
-        for (let i = 0; i < projets.length; i++) {
-            const figure = document.createElement("figure");
-            gallerie.appendChild(figure);
-            const image = document.createElement("img");
-            image.src = projets[i].imageUrl;
-            figure.appendChild(image);
-            const figcaption = document.createElement("figcaption");
-            figcaption.innerText = projets[i].title;
-            figure.appendChild(figcaption);
+        function projetsAffiches(projets) {
+            for (let i = 0; i < projets.length; i++) {
+                const figure = document.createElement("figure");
+                figure.id = projets[i].id;
+                figure.categoryId = projets[i].categoryId;
+                gallerie.appendChild(figure);
+                const image = document.createElement("img");
+                image.src = projets[i].imageUrl;
+                figure.appendChild(image);
+                const figcaption = document.createElement("figcaption");
+                figcaption.innerText = projets[i].title;
+                figure.appendChild(figcaption);
+            }
         }
+        projetsAffiches(projets);
+
+        // Récupère les catégories pour le tri des projets
+        fetch ("http://localhost:5678/api/categories")
+        .then(function(reponse) {
+            return reponse.json();
+        })
+        .then(function(categories) {
+            let filtres = document.querySelector(".filtres");
+            const bouton = document.createElement("input");
+                filtres.appendChild(bouton);
+                bouton.type = 'button';
+                bouton.value = "Tous";
+                bouton.id = 'btn-tous';
+            for (let i = 0; i < categories.length; i++) {
+                const bouton = document.createElement("input");
+                filtres.appendChild(bouton);
+                bouton.type = 'button';
+                bouton.value = categories[i].name;
+                bouton.id = `btn-${categories[i].name}`;
+            }
+
+            const boutonTous = document.getElementById("btn-tous");
+            boutonTous.addEventListener("click", function () {
+                const tous = projets.filter(function (projet) {
+                    return projet.categoryId;
+                });
+                document.querySelector(".gallery").innerHTML = "";
+                projetsAffiches(tous);
+            });
+
+            const boutonObjets = document.getElementById("btn-Objets");
+            boutonObjets.addEventListener("click", function () {
+                const objets = projets.filter(function (projet) {
+                    return projet.categoryId === 1;
+                });
+                document.querySelector(".gallery").innerHTML = "";
+                projetsAffiches(objets);
+            });
+
+            const boutonAppartements = document.getElementById("btn-Appartements");
+            boutonAppartements.addEventListener("click", function () {
+                const appartements = projets.filter(function (projet) {
+                    return projet.categoryId === 2;
+                });
+                document.querySelector(".gallery").innerHTML = "";
+                projetsAffiches(appartements);
+            });
+
+            const boutonHotels = document.getElementById("btn-Hotels & restaurants");
+            boutonHotels.addEventListener("click", function () {
+                const hotels = projets.filter(function (projet) {
+                    return projet.categoryId === 3;
+                });
+                document.querySelector(".gallery").innerHTML = "";
+                projetsAffiches(hotels);
+            });
+        });
     });
+
+
+
+
+    
